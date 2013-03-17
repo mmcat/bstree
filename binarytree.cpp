9077ad8d96c3e8b50d201d647651206ca72b4c4a
mingming <mingmingfly@gmail.com>
2013-03-16 20:59:19 -0400

    bb

--- binarytree.cpp
+++ binarytree.cpp
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  03/14/2013 20:59:18
  *       Revision:  none
- *       Compiler:  gcc
+ *       Compiler:  g++
  *
  *         Author:  YOUR NAME (), 
  *   Organization:  
